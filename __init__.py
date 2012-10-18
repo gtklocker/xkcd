22a212ae5728fc4f17c18762d1e20f64fce09ad0
Kostis Karantias <karantiaskostis@gmail.com>
2012-10-18 17:57:08 -0400

    Docstring changes.

--- __init__.py
+++ __init__.py
@@ -1,11 +1,12 @@
-"""Tiny library to grab XKCD comic information."""
+"""This module exposes the XKCD API to Python."""
 import urllib2
 import json
 import datetime
 
 class Comic():
+    """Get information about comic #num."""
     def __init__(self, num):
-        "Obtains all data about the comic."
+        """Grab the data about the comic and save it."""
         self.num = num
         self.api = 'https://xkcd.com/%s/info.0.json' % self.num
         self.url = 'https://xkcd.com/%s' % self.num
@@ -17,37 +18,37 @@ class Comic():
 
     @property
     def num(self):
-        "Return the comic's number."
+        """Return the comic's number."""
         return self.num
 
     @property
     def url(self):
-        "Return the comic's url."
+        """Return the comic's url."""
         return self.url
 
     @property
     def img(self):
-        "Return the comic's image url."
+        """Return the comic's image url."""
         return self.data['img']
 
     @property
     def title(self):
-        "Return the comic's title."
+        """Return the comic's title."""
         return self.data['title']
 
     @property
     def alt(self):
-        "Return the comic's alt."
+        """Return the comic's alt."""
         return self.data['alt']
 
     @property
     def transcript(self):
-        "Return the comic's transcript."
+        """Return the comic's transcript."""
         return self.data['transcript']
 
     @property
     def when(self):
-        "Return when the comic was posted."
+        """Return when the comic was posted."""
         return datetime.date(int(self.data['year']),
                              int(self.data['month']),
                              int(self.data['day']))
