61158b7ce9223e27942aa830321bf4a91e6675bc
Kostis Karantias <karantiaskostis@gmail.com>
2012-10-18 17:27:15 -0400

    All properties are set, coding style change.

--- xkcd.py
+++ xkcd.py
@@ -3,31 +3,51 @@ import urllib2
 import json
 import datetime
 
-class xkcdComic:
-    def __init__( self, num ):
+class Comic():
+    def __init__(self, num):
+        "Obtains all data about the comic."
         self.num = num
         self.api = 'https://xkcd.com/%s/info.0.json' % self.num
         self.url = 'https://xkcd.com/%s' % self.num
 
         try:
-            self.data = json.loads( urllib2.urlopen( self.api ).read() )
+            self.data = json.loads(urllib2.urlopen(self.api).read())
         except:
-            print( 'Something\'s wrong with the internet.' )
+            print('Something\'s wrong with the internet.')
 
     @property
-    def num( self ):
+    def num(self):
+        "Return the comic's number."
         return self.num
-    def url( self ):
+
+    @property
+    def url(self):
+        "Return the comic's url."
         return self.url
-    def img( self ):
-        return self.data[ 'img' ]
-    def title( self ):
-        return self.data[ 'title' ]
-    def alt( self ):
-        return self.data[ 'alt' ]
-    def transcript( self ):
-        return self.data[ 'transcript' ]
-    def when( self ):
-        return datetime.date( int( self.data[ 'year' ] ), \
-                              int( self.data[ 'month' ] ), \
-                              int( self.data[ 'day' ] ) )
+
+    @property
+    def img(self):
+        "Return the comic's image url."
+        return self.data['img']
+
+    @property
+    def title(self):
+        "Return the comic's title."
+        return self.data['title']
+
+    @property
+    def alt(self):
+        "Return the comic's alt."
+        return self.data['alt']
+
+    @property
+    def transcript(self):
+        "Return the comic's transcript."
+        return self.data['transcript']
+
+    @property
+    def when(self):
+        "Return when the comic was posted."
+        return datetime.date(int(self.data['year']),
+                             int(self.data['month']),
+                             int(self.data['day']))
